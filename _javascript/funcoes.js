d2f05c5be2891fe502dcc80675082110f3873337
danielsfr <danielsfr1991@gmail.com>
2018-08-19 12:05:09 -0300

    Initial commit

---
+++ _javascript/funcoes.js
@@ -0,0 +1,11 @@
+   function mudafoto (foto) {
+        document.getElementById("icone").src=foto;
+
+
+    }
+   function calc_total(par) {
+       var qtd = parseInt(document.getElementById(cQtd.value);
+       var tot = qtd * 1500;
+       document.getElementById(cTot.value = tot);
+
+   }
